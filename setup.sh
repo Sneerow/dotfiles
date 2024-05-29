27a8ab96b1264abd058c0a5fdc397fb696d8a28b
Loup Dallier <loup.dallier@epita.fr>
2024-05-29 09:43:35 +0200

    Update setup script

--- setup.sh
+++ setup.sh
@@ -1,10 +1,24 @@
 #!/bin/sh
 
-# ln -st ~/.config/ ~/.dotfiles/alacritty/
-# ln -st ~/.config/ ~/.dotfiles/autorandr/
-ln -st ~/.config/ ~/.dotfiles/fish/
-ln -st ~/.config/ ~/.dotfiles/git/
-# ln -st ~/.config/ ~/.dotfiles/i3/
-ln -st ~/.config/ ~/.dotfiles/pypoetry/
-# ln -st ~/.config/ ~/.dotfiles/vim/
-ln -st ~/.config/ ~/.dotfiles/starship.toml
+# TODO add a function to automate link creation
+
+eval "$(/opt/homebrew/bin/brew shellenv)"
+
+ln -s ~/.dotfiles/fish ~/.config/fish
+
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
+nvm install --lts
+
+ln -s ~/.dotfiles/nvim ~/.config/nvim
+ln -s ~/.dotfiles/git ~/.config/git
+ln -s ~/.dotfiles/starship.toml ~/.config/starship.toml
+ln -s ~/.dotfiles/bat ~/.config/bat
+ln -s ~/.dotfiles/wezterm ~/.config/wezterm
+
+# Update bat cache (for theme)
+bat cache --build
+
+pyenv global 3.12.3
+
+
+# Maybe update /etc/hosts to comment ::1 localhost
